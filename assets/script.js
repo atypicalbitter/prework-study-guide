db1dbfff6e8f13512aecb29fae28d3e3452c78eb
Matt Dudman <mattdudman95@googlemail.com>
2023-11-07 09:29:52 +0000

    saving via terminal, have created javascript file

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var topic = "HTML";
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
